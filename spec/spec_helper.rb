88e9af5177f2f661545b96d5acb2585097d86932
fltiago <fltiago@gmail.com>
2013-03-21 14:17:29 -0300

    Adicionando faraday e webmock ao testes, assim como desabilitando as conexões a cada teste.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,15 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 #
 require "vis_client"
-require "eventmachine"
-require "em-http"
+require "faraday"
+require "webmock/rspec"
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+
+  config.before do
+    WebMock.disable_net_connect!
+  end
 end
