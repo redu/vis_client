2c8e6aa490450d5791f12e799a213b0b383f5077
fltiago <fltiago@gmail.com>
2013-03-21 14:32:11 -0300

    Adição da classe adapter responsável por enviar e configurar o envio as requisições. :+1:

---
+++ lib/vis_client/adapter.rb
@@ -0,0 +1,63 @@
+require 'base64'
+require 'json'
+
+module VisClient
+  class Adapter
+
+    def initialize(opts = nil)
+      @config = VisClient.config
+      @config.config.import(opts) if opts
+    end
+
+    def send_request(params, url)
+      if config.deliver_notifications
+        faraday_request params, url
+      else
+        config.logger.info "Notifications are disabled, if you wanna see some notifications," \
+          "please modify the configuration."
+      end
+    end
+
+
+    private
+
+    attr_reader :config
+
+    def faraday_request(params, url)
+      resp = connection.post(url) do |c|
+        c.body = params.to_json
+      end
+
+      handle_response(resp.status, resp.body)
+    end
+
+    def connection
+      headers = {'Authorization' => Base64::encode64("core-team:JOjLeRjcK"),
+                 'Content-Type' => 'application/json'}
+      @faraday ||= Faraday.new(:url => config.endpoint,
+                               :headers => headers)
+    end
+
+    def handle_response(status_code, body)
+      case status_code
+      when 200
+        return true
+      when 201
+        return true
+      when 202
+        return true
+      when 400
+        raise BadRequest, "Bad request"
+      when 401
+        raise UnauthorizedAccess, "Not Authorized to access vis server."
+      when 404
+        raise ResourceNotFound, "Resource not found: app_id is probably invalid"
+      when 409
+        raise Conflict, "Resorce already exists"
+      else
+        raise ConnectionError, "Unknown error (status code #{status_code}): #{body}"
+      end
+    end
+
+  end
+end
---
+++ spec/vis_client/adapter_spec.rb
@@ -0,0 +1,68 @@
+require "spec_helper"
+
+module VisClient
+  describe Adapter do
+    let(:auth) { Base64::encode64("core-team:JOjLeRjcK").chomp }
+    let(:headers) { {'Authorization' => auth,
+                     'Content-Type'=>'application/json'} }
+    let(:params) { {:test => "test"} }
+
+    context "with default configurations" do
+      subject { Adapter.new }
+
+      it "should POST to a default endpoint" do
+        stub_request(:post, VisClient.config.endpoint).
+          with(:body => params.to_json, :headers => headers).
+          to_return(:status => 201, :body => "", :headers => {})
+
+        subject.send_request(params, "")
+
+        a_request(:post, VisClient.config.endpoint).
+          with(:body => params.to_json, :headers => headers).
+          should have_been_made
+      end
+    end
+
+    context "with customized configurations" do
+      let(:endpoint) { "http://localhost:4000" }
+      let(:action) { "/hierarchy_notifications.json" }
+      let(:url) { endpoint + action }
+
+      context "like different endpoint" do
+        subject { Adapter.new("endpoint" => endpoint) }
+
+        it "should POST to specific endpoint" do
+
+          stub_request(:post, url).
+            with(:body => params.to_json, :headers => headers).
+            to_return(:status => 201, :body => "", :headers => {})
+
+          subject.send_request(params, action)
+
+          a_request(:post, url).
+            with(:body => params.to_json, :headers => headers).
+            should have_been_made
+        end
+      end
+
+      context "like deliver notification to false" do
+        subject { Adapter.new("deliver_notifications" => false) }
+
+        it "shouldn't POST to specific endpoint" do
+
+          subject.send_request(params, action)
+
+          a_request(:post, url).
+            with(:body => params.to_json, :headers => headers).
+            should_not have_been_made
+        end
+
+        it "should logger a message" do
+          Logger.any_instance.should_receive(:info).with(/Notifications are disabled/)
+
+          subject.send_request(params, action)
+        end
+      end
+    end
+  end
+end
